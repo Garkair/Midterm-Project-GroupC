ac1407251ef47e2235ca6d967b42eb10e59148b3
JD <juandavidsilvera@hotmail.com>
2026-03-03 00:42:50 -0600

    Fixed file tree and deleted files from previous projects that got pushed out by accident.

---
+++ exercise4.py
@@ -0,0 +1,10 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+
+add = int(num1) + int(num2)
+sub = int(num1) - int(num2)
+mul = int(num1) * int(num2)
+div = int(num1) / int(num2)
+mod = int(num1) % int(num2)
+
+print(f"Addition result: {add}, Substraction result: {sub}, Multiplication result: {mul}, division result: {div}, and modulus result is {mod}.")
