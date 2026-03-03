cdbd6c7834e6a513ae6eea80c08ccd159e3e7df6
JD <juandavidsilvera@hotmail.com>
2026-03-03 00:47:03 -0600

    Fixed file tree and deleted files from previous projects that got pushed out by accident.

--- Submission 3/logger.py
+++
@@ -1,7 +0,0 @@
-class Logger:
-    def __init__(self, filename):
-        self.filename = filename
-
-    def write_line(self, message):
-        with open(self.filename, "a") as file:
-            file.write(message + "\n")
--- helper.py
+++ helper.py
@@ -6,26 +6,26 @@ import random
 
 # Dictionary containing 100 BTC BIP-39 seed words
 word_dict = {
-    0: "abandon", 1: "ability", 2: "able", 3: "about", 4: "above",
-    5: "absent", 6: "absorb", 7: "abstract", 8: "absurd", 9: "abuse",
-    10: "access", 11: "accident", 12: "account", 13: "accuse", 14: "achieve",
-    15: "acid", 16: "acoustic", 17: "acquire", 18: "across", 19: "act",
-    20: "action", 21: "actor", 22: "actress", 23: "actual", 24: "adapt",
-    25: "add", 26: "addict", 27: "address", 28: "adjust", 29: "admit",
-    30: "adult", 31: "advance", 32: "advice", 33: "aerobic", 34: "affair",
-    35: "afford", 36: "afraid", 37: "again", 38: "age", 39: "agent",
-    40: "agree", 41: "ahead", 42: "aim", 43: "air", 44: "airport",
-    45: "aisle", 46: "alarm", 47: "album", 48: "alcohol", 49: "alert",
-    50: "alien", 51: "all", 52: "alley", 53: "allow", 54: "almost",
-    55: "alone", 56: "alpha", 57: "already", 58: "also", 59: "alter",
-    60: "always", 61: "amateur", 62: "amazing", 63: "among", 64: "amount",
-    65: "amused", 66: "analyst", 67: "anchor", 68: "ancient", 69: "anger",
-    70: "angle", 71: "angry", 72: "animal", 73: "ankle", 74: "announce",
-    75: "annual", 76: "another", 77: "answer", 78: "antenna", 79: "antique",
-    80: "anxiety", 81: "any", 82: "apart", 83: "apology", 84: "appear",
-    85: "apple", 86: "approve", 87: "april", 88: "arch", 89: "arctic",
-    90: "area", 91: "arena", 92: "argue", 93: "arm", 94: "armed",
-    95: "armor", 96: "army", 97: "around", 98: "arrange", 99: "arrest"
+    0: "oxygen", 1: "castle", 2: "gravity", 3: "dolphin", 4: "forest",
+    5: "guitar", 6: "helmet", 7: "island", 8: "jungle", 9: "kingdom",
+    10: "lemon", 11: "magnet", 12: "nephew", 13: "orbit", 14: "pencil",
+    15: "quantum", 16: "rocket", 17: "satoshi", 18: "tunnel", 19: "unfair",
+    20: "velvet", 21: "whisper", 22: "yellow", 23: "zebra", 24: "artist",
+    25: "balance", 26: "camera", 27: "desert", 28: "energy", 29: "fabric",
+    30: "galaxy", 31: "harbor", 32: "ignore", 33: "jacket", 34: "kitten",
+    35: "ladder", 36: "manual", 37: "nature", 38: "object", 39: "planet",
+    40: "quiet", 41: "rescue", 42: "shadow", 43: "talent", 44: "update",
+    45: "vacuum", 46: "window", 47: "youth", 48: "anchor", 49: "battle",
+    50: "circle", 51: "damage", 52: "escape", 53: "future", 54: "globe",
+    55: "hammer", 56: "impact", 57: "jewel", 58: "knife", 59: "launch",
+    60: "memory", 61: "normal", 62: "ocean", 63: "palace", 64: "random",
+    65: "school", 66: "travel", 67: "unique", 68: "victory", 69: "wealth",
+    70: "zone", 71: "brick", 72: "credit", 73: "drama", 74: "elite",
+    75: "flame", 76: "giant", 77: "honor", 78: "index", 79: "judge",
+    80: "legend", 81: "motion", 82: "novel", 83: "option", 84: "pride",
+    85: "rare", 86: "signal", 87: "theme", 88: "urban", 89: "value",
+    90: "wolf", 91: "bonus", 92: "climb", 93: "dream", 94: "event",
+    95: "focus", 96: "green", 97: "host", 98: "iron", 99: "logic"
 }
 
 selected_word = ""
